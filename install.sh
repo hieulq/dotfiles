4739bd59a303b416d0e8d39fd61ea0e031fa815a
Hieu LE <hieulq@vn.fujitsu.com>
2017-12-15 15:29:46 +0700

    update alias cli for path

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+# vim plugins
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/
 vim +PluginInstall +qall
+
+# git alias
 cat git_alias >> ~/.gitconfig
+
+# aliasme setup
+mkdir ~/.aliasme
+curl https://raw.githubusercontent.com/hieulq/aliasme/master/aliasme.sh > ~/.aliasme/aliasme.sh
+echo "source ~/.aliasme/aliasme.sh" >> ~/.bash_profile
+source ~/.bash_profile
+
+# setup basic alias
+echo "alias g='git'" >> ~/.profile
+source ~/.profile
