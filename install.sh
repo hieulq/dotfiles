fdfec793978ab3c17bd715190ff837ba603ed200
Hieu LE <hieulq19@gmail.com>
2018-01-01 20:34:31 +0700

    add script for Mac

--- install.sh
+++ install.sh
@@ -1,19 +1,55 @@
 #!/bin/bash
 
-# vim plugins
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp .vimrc ~/
-vim +PluginInstall +qall
+if [ "$(uname)" == "Darwin" ]; then
+    echo "Setting up my Mac..."
+    # Check for Homebrew and install if we don't have it
+    if test ! $(which brew); then
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+    # Update Homebrew recipes
+    brew update
 
-# git alias
-cat git_alias >> ~/.gitconfig
+    # Install all our dependencies with bundle (See Brewfile)
+    brew tap homebrew/bundle
+    brew bundle
+    brew cleanup
 
-# aliasme setup
-mkdir ~/.aliasme
-curl https://raw.githubusercontent.com/hieulq/aliasme/master/aliasme.sh > ~/.aliasme/aliasme.sh
-echo "source ~/.aliasme/aliasme.sh" >> ~/.bash_profile
-source ~/.bash_profile
+    # Install powerline patched-fonts
+    source powerline-install.sh
 
-# setup basic alias
-echo "alias g='git'" >> ~/.profile
-source ~/.profile
+    # Make ZSH the default shell environment
+    chsh -s $(which zsh)
+
+    # vim plugins
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+    # Copy default config
+    cp .vimrc .aliases .zshrc .curlrc .editorconfig .exports .functions .gitconfig .gitignore .inputrc .vimrc .wgetrc ~/
+
+    # Install vim plugins
+    vim +PluginInstall +qall
+
+    # Install alas
+    sudo -E pip install alas    
+
+    # Set macOS preferences
+    # We will run this last because this will reload the shell
+    source .macos
+
+    # Install oh-my-zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Install powerline patched-fonts
+    sudo apt-get install fonts-powerline
+    # vim plugins
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    cp .vimrc ~/
+    vim +PluginInstall +qall
+
+    # git alias
+    cat git_alias >> ~/.gitconfig
+
+    # setup basic alias
+    echo "alias g='git'" >> ~/.profile
+    source ~/.profile
+fi
