dcdb46ae65f267f66a249fdd0adf2710c2efdd43
Hieu LE <hieulq@Hieus-MacBook-Pro.local>
2018-06-06 19:20:31 +0700

    update

--- install.sh
+++ install.sh
@@ -7,19 +7,16 @@ if [ "$(uname)" == "Darwin" ]; then
         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     fi
     # Update Homebrew recipes
-    brew update
+    #brew update
 
     # Install all our dependencies with bundle (See Brewfile)
-    brew tap homebrew/bundle
-    brew bundle
-    brew cleanup
+    #brew tap homebrew/bundle
+    #brew bundle
+    #brew cleanup
 
     # Install powerline patched-fonts
     # source powerline-install.sh
-    source nerd-install.sh
-
-    # Make ZSH the default shell environment
-    chsh -s $(which zsh)
+    #source nerd-install.sh
 
     # vim plugins
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -28,17 +25,19 @@ if [ "$(uname)" == "Darwin" ]; then
     vim +PluginInstall +qall
 
     # Install alas
-    sudo -E pip install alas    
+    #sudo -E pip install alas
 
     # Set macOS preferences
     # We will run this last because this will reload the shell
-    source .macos
+    #source .macos
 
     # Install oh-my-zsh
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 
+    # Make ZSH the default shell environment
+    #chsh -s $(which zsh)
     # Copy default config
     cp .vimrc .aliases .zshrc .curlrc .editorconfig .exports .functions .gitconfig .gitignore .inputrc .wgetrc ~/
 
@@ -51,12 +50,12 @@ if [ "$(uname)" == "Darwin" ]; then
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     # Install hh and zsh
     sudo add-apt-repository ppa:ultradvorka/ppa -y
-    sudo apt-get update 
+    sudo apt-get update
     sudo apt-get install zsh thefuck tree hh -y
 
     # Install powerline patched-fonts
     # source powerline-install.sh
-    source nerd-install.sh
+    #source nerd-install.sh
 
     # Copy default config
     cp .vimrc .aliases .zshrc .curlrc .editorconfig .exports .functions .gitconfig .gitignore .inputrc .vimrc .wgetrc ~/
